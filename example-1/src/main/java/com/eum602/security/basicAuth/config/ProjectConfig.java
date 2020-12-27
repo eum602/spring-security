4ed980ff7f3225d43b2f35798fbe08d15ee8e326
Erick <eum602@gmail.com>
2020-12-27 17:42:23 -0500

    adds a second approach ==> uses overriden methods to configure the userDetailsService and injects a new one there. This approach replaces the use of beans

--- example-1/src/main/java/com/eum602/security/basicAuth/config/ProjectConfig.java
+++ example-1/src/main/java/com/eum602/security/basicAuth/config/ProjectConfig.java
@@ -1,42 +1,34 @@
 package com.eum602.security.basicAuth.config;
 
-import org.springframework.cglib.proxy.NoOp;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration //Configuration class
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
-    //By adding this configuration the default password is not generated anymore
-    @Bean //Inject the returned value as a bean in the spring context
-    public UserDetailsService userDetailsService(){
-        UserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-        UserDetails user =  User.withUsername("eum602")
-                .password("password")
-                .authorities("read")
-                .build();
-        userDetailsService.createUser(user);
-        return userDetailsService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();//Treats passwords as plain texts
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.httpBasic();
         http.authorizeRequests()
                 .anyRequest().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        UserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails user =  User.withUsername("eum602")
+                .password("password")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
+        auth.userDetailsService(userDetailsService).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
