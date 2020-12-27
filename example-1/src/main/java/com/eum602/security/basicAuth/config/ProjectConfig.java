69d7f6eb59531c8fc2f9ab77c7341b3c86b856d7
Erick <eum602@gmail.com>
2020-12-27 17:21:42 -0500

    overrides default websecurity ==> allows all incoming requrest without requiring authentication

--- example-1/src/main/java/com/eum602/security/basicAuth/config/ProjectConfig.java
+++ example-1/src/main/java/com/eum602/security/basicAuth/config/ProjectConfig.java
@@ -3,6 +3,9 @@ package com.eum602.security.basicAuth.config;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,7 +15,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration //Configuration class
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
     //By adding this configuration the default password is not generated anymore
     @Bean //Inject the returned value as a bean in the spring context
     public UserDetailsService userDetailsService(){
@@ -28,5 +31,12 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();//Treats passwords as plain texts
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.httpBasic();
+        http.authorizeRequests()
+                .anyRequest().permitAll();
     }
 }
