bc34b77b912295590a7b7e2e8c4afcb8f2c6af97
Erick <eum602@gmail.com>
2020-12-28 13:54:56 -0500

    adds first version of SingleUserDetailsService

---
+++ example-1/src/main/java/com/eum602/security/basicAuth/config/single/SingleStudentUserDetails.java
@@ -0,0 +1,44 @@
+package com.eum602.security.basicAuth.config.single;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SingleStudentUserDetails implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "READ");
+    }
+
+    @Override
+    public String getPassword() {
+        return "password";
+    }
+
+    @Override
+    public String getUsername() {
+        return "eum602";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
