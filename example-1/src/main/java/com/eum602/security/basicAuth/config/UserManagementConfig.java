a9ce90094b1bf5c633d90bed5c03c3932089e090
Erick <eum602@gmail.com>
2020-12-28 21:48:17 -0500

    configures JdbcUserDetailsManager so it is now connecteed to a postgresql database

--- example-1/src/main/java/com/eum602/security/basicAuth/config/UserManagementConfig.java
+++ example-1/src/main/java/com/eum602/security/basicAuth/config/UserManagementConfig.java
@@ -1,24 +1,28 @@
 package com.eum602.security.basicAuth.config;
 
-import com.eum602.security.basicAuth.config.single.SingleStudentUserDetails;
-import com.eum602.security.basicAuth.services.inmemory.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 public class UserManagementConfig {//Adds only beans responsible for managing the user
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user1 = new SingleStudentUserDetails("eum602","password","user");
-        UserDetails user2 = new SingleStudentUserDetails("r2d2","password","user");
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        String usersByUserameQuery = "select username, password, enabled from users where username = ?"; //requires the three values in order to work properly
+        String authsByUserQuery = "select username, authority from users where username = ?";
+        JdbcUserDetailsManager userDetailsManager= new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUserameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
+        /*UserDetails user1 = new SingleStudentUserDetails("eum602","password","read");
+        UserDetails user2 = new SingleStudentUserDetails("r2d2","password","write");
         List<UserDetails> users = List.of(user1,user2);
-        return new InMemoryUserDetailsService(users);
+        return new InMemoryUserDetailsService(users);*/
     }
 
     @Bean
