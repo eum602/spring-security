750b52498f0fc71ed689aefc1427c8ddc164278c
Erick <eum602@gmail.com>
2020-12-28 21:54:50 -0500

    adds UserDetails implementations for SQL databases, not configured with userDetailsService but it is ready to be used

---
+++ example-1/src/main/java/com/eum602/security/basicAuth/config/database/SecuritySQLDatabaseUser.java
@@ -0,0 +1,53 @@
+package com.eum602.security.basicAuth.config.database;
+
+import com.eum602.security.basicAuth.DAO.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Wraps JPA according to UserDetails interface
+ */
+public class SecuritySQLDatabaseUser implements UserDetails {
+    private final User user;
+
+    public SecuritySQLDatabaseUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
